12fe5a2b6765d04f3538c9827f9d4d3a430c808e
Ivan Shvedunov <ivan4th@gmail.com>
2014-09-20 22:03:43 +0400

    WBHomaBridge: auto-grab controls from MQTT.

--- hardware/WBHomaBridge.cpp
+++ hardware/WBHomaBridge.cpp
@@ -1,16 +1,65 @@
-// TBD: uninit lib
-// TBD: make keepalive configurable
-// TBD: move mqtt loop timeout to a constant
-// TBD: obtain version from git repository, too (during build)
-// TBD: Mon Aug 25 00:44:30 2014 Error: zzz hardware (3) thread seems to have ended unexpectedly
-//      (though the thread still works)
 #include <ctime>
-#include "stdafx.h"   
+#include <vector>
+#include <boost/algorithm/string/split.hpp>
+#include <boost/algorithm/string.hpp>
+#include "stdafx.h"
 #include "DomoticzHardware.h"
 #include "WBHomaBridge.h"
 #include "../main/Logger.h"
 #include "../main/SQLHelper.h"
+#include "../main/Helper.h"
+#include "../main/RFXtrx.h" // more hw types
 #include "hardwaretypes.h"
+
+class MQTTTypeHandler
+{
+public:
+    MQTTTypeHandler(const char* type): m_type(type) {}
+    virtual ~MQTTTypeHandler();
+    virtual bool HandleValue(const std::string& inputValue,
+                             int* out_type, int* out_subtype,
+                             int* out_nValue, std::string* out_sValue) = 0;
+    bool Match(const std::string& type) const;
+private:
+    const char* m_type;
+};
+
+MQTTTypeHandler::~MQTTTypeHandler()
+{
+    // NOOP
+}
+
+bool MQTTTypeHandler::Match(const std::string& type) const
+{
+    return m_type  == type;
+}
+
+class MQTTTemperatureHandler: public MQTTTypeHandler
+{
+public:
+    MQTTTemperatureHandler(): MQTTTypeHandler("temperature") {}
+    bool HandleValue(const std::string& inputValue,
+                     int* out_type, int* out_subtype,
+                     int* out_nValue, std::string* out_sValue);
+};
+
+bool MQTTTemperatureHandler::HandleValue(const std::string& inputValue,
+                                         int* out_type, int* out_subtype,
+                                         int* out_nValue, std::string* out_sValue)
+{
+    *out_type = pTypeTEMP;
+    *out_subtype = sTypeTEMP1;
+    *out_nValue = 0;
+    *out_sValue = inputValue;
+    return true;
+}
+
+namespace {
+    MQTTTypeHandler* type_handlers[] = {
+        new MQTTTemperatureHandler(),
+        0
+    };
+}
 
 class MQTTHandler: public mosqpp::mosquittopp
 {
@@ -21,11 +70,11 @@ public:
     void on_subscribe(int mid, int qos_count, const int *granted_qos);
 private:
     WBHomaBridge* m_bridge;
-    const int keepalive = 60;
+    const int keepalive;
 };
 
 MQTTHandler::MQTTHandler(WBHomaBridge* bridge, std::string host, int port)
-    : m_bridge(bridge)
+    : m_bridge(bridge), keepalive(60)
 {
     connect(host.c_str(), port, keepalive);
 }
@@ -34,27 +83,48 @@ void MQTTHandler::on_connect(int rc)
 {
 	_log.Log(LOG_STATUS, "WBHomaBridge: connected to MQTT broker with code %d", rc);
     if (rc == 0)
-        // subscribe(0, "#");
-        subscribe(0, "/devices/msu34tlp/controls/Pressure");
+        subscribe(0, "#");
+        // subscribe(0, "/devices/notebook/controls/Core0");
+        // subscribe(0, "/devices/msu34tlp/controls/Pressure");
 }
 
 void MQTTHandler::on_message(const struct mosquitto_message *message)
 {
     _log.Log(LOG_STATUS, "message topic: %s payload: %s", message->topic, message->payload);
-    if (strcmp(message->topic, "/devices/msu34tlp/controls/Pressure"))
+
+    bool is_type = false;
+    std::vector<std::string> parts;
+    StringSplit(message->topic, "/", parts);
+    if ((parts.size() != 4 && parts.size() != 6) ||
+        parts[0] != "devices" ||
+        parts[2] != "controls") {
+        _log.Log(LOG_STATUS, "ignoring message with topic: %s", message->topic);
         return;
+    }
+    if (parts.size() == 6) {
+        if (parts[4] != "meta" || parts[5] != "type") {
+            _log.Log(LOG_STATUS, "ignoring message with topic: %s", message->topic);
+            return;
+        }
+        is_type = true;
+    }
+    MQTTAddress address(parts[1], parts[3]);
+    m_bridge->HandleMQTTMessage(address, is_type, (const char*)message->payload);
 
-	char buf[51];
-    memset(buf, 0, 51);
-    memcpy(buf, message->payload, 50);
-    double v = atof(buf);
+    // if (strcmp(message->topic, "/devices/msu34tlp/controls/Pressure"))
+    //     return;
 
-    _tGeneralDevice gDevice;
-    gDevice.subtype = sTypePressure;
-    gDevice.id = 1;
-    gDevice.floatval1 = v;
-    gDevice.intval1 = 1; // device index
-    m_bridge->sDecodeRXMessage(m_bridge, (const unsigned char *)&gDevice);
+	// char buf[51];
+    // memset(buf, 0, 51);
+    // memcpy(buf, message->payload, 50);
+    // double v = atof(buf);
+
+    // _tGeneralDevice gDevice;
+    // gDevice.subtype = sTypePressure;
+    // gDevice.id = 1;
+    // gDevice.floatval1 = v;
+    // gDevice.intval1 = 1; // device index
+    // m_bridge->sDecodeRXMessage(m_bridge, (const unsigned char *)&gDevice);
 }
 
 void MQTTHandler::on_subscribe(int, int, const int*)
@@ -77,8 +147,28 @@ WBHomaBridge::~WBHomaBridge()
     // NOOP
 }
 
-void WBHomaBridge::WriteToHardware(const char *pdata, const unsigned char length)
+void WBHomaBridge::WriteToHardware(const char *, const unsigned char)
 {
+    // TBD
+}
+
+void WBHomaBridge::HandleMQTTMessage(const MQTTAddress& address, bool is_type, const std::string& payload)
+{
+    ValueMap::iterator it = m_values.find(address);
+    if (it == m_values.end()) {
+        m_values[address] = is_type ? MQTTValue("", payload) : MQTTValue(payload, "");
+        return;
+    }
+    if (is_type)
+        it->second.type = payload;
+    else
+        it->second.value = payload;
+    _log.Log(LOG_NORM, "WBHomaBridge: got message: sys %s ctrl %s payload %s is_type %s ready %s",
+             address.system_id.c_str(), address.device_control_id.c_str(),
+             payload.c_str(), is_type ? "true" : "false",
+             it->second.ready() ? "true" : "false");
+    if (it->second.ready())
+        WriteValueToDB(it->first, it->second);
 }
 
 bool WBHomaBridge::StartHardware()
@@ -104,22 +194,35 @@ bool WBHomaBridge::StopHardware()
     return true;
 }
 
-bool WBHomaBridge::DeviceExists()
+void WBHomaBridge::WriteValueToDB(const MQTTAddress& address, const MQTTValue& value)
 {
-	std::stringstream szQuery;
-	std::vector<std::vector<std::string> > result;
-	szQuery << "SELECT Name FROM DeviceStatus WHERE (HardwareID==" << m_HwdID << ") AND (DeviceID=='" << 1 << "') AND (Type==" << int(pTypeGeneral) << ") AND (Subtype==" << int(sTypePressure) << ")";
-	result = m_sql.query(szQuery.str());
-	return result.size() > 0;
-}
+    bool found = false;
+    int devType, subType, nValue = 0;
+    std::string sValue = "";
+    for (MQTTTypeHandler** h = type_handlers; *h; ++h) {
+        if ((*h)->Match(value.type)) {
+            found = true;
+            (*h)->HandleValue(value.value, &devType, &subType, &nValue, &sValue);
+            break;
+        }
+    }
+    if (!found) {
+        _log.Log(LOG_ERROR, "no matching type for MQTT/homA type %s", value.type.c_str());
+        return;
+    }
 
-void WBHomaBridge::UpdateDeviceName()
-{
-	std::stringstream szQuery;
-    szQuery.clear();
-    szQuery.str("");
-    szQuery << "UPDATE DeviceStatus SET Name='" << "WBHomaBridgeValue" << "' WHERE (HardwareID==" << m_HwdID << ") AND (DeviceID=='" << 1 << "') AND (Type==" << int(pTypeGeneral) << ") AND (Subtype==" << int(sTypePressure) << ")";
-    m_sql.query(szQuery.str());
+    _log.Log(LOG_NORM, "WBHomaBridge: writing param: hw id %d, devid %s, nValue %d, sValue %s",
+             m_HwdID, address.GetDeviceID().c_str(), nValue, sValue.c_str());
+    std::string devname;
+    m_sql.UpdateValue(m_HwdID, address.GetDeviceID().c_str(),
+                      1, devType, subType, 12, 255, nValue, sValue.c_str(),
+                      devname);
+    int r = m_sql.execute("UPDATE DeviceStatus SET Name = ? "
+                          "WHERE HardwareID = ? AND DeviceID = ? AND Name <> ?",
+                          SQLParamList() << address.device_control_id << m_HwdID <<
+                          address.GetDeviceID() << address.device_control_id);
+    _log.Log(LOG_NORM, "WBHomaBridge: name update result: %d", r);
+    return;
 }
 
 void WBHomaBridge::Do_Work()
@@ -131,31 +234,20 @@ void WBHomaBridge::Do_Work()
         int rc = handler.loop(100);
         if (rc)
             handler.reconnect();
-		// sleep(1);
-		// time_t t = time(0);
-
-        // bool bDeviceExists = DeviceExists();
-
-        // _tGeneralDevice gDevice;
-        // gDevice.subtype = sTypePercentage;
-        // gDevice.id = 1;
-        // gDevice.floatval1 = t % 101;
-        // gDevice.intval1 = 1; // device index
-        // sDecodeRXMessage(this, (const unsigned char *)&gDevice);
-
-        // if (!bDeviceExists)
-        //     UpdateDeviceName();
-
-		// if (ltime.tm_sec%POLL_INTERVAL==0)
-		// {
-		// 	FetchData();
-		// }
-		// if (ltime.tm_sec % 12 == 0) {
-		// 	mytime(&m_LastHeartbeat);
-		// }
-
 	}
 
-	_log.Log(LOG_STATUS,"WBHomaBridge: Stopped...");			
+	_log.Log(LOG_STATUS,"WBHomaBridge: Stopped...");
 }
 
+// TBD: uninit lib
+// TBD: make keepalive configurable
+// TBD: move mqtt loop timeout to a constant
+// TBD: obtain version from git repository, too (during build)
+// TBD: make sure notifications work
+// TBD: Mon Aug 25 00:44:30 2014 Error: zzz hardware (3) thread seems to have ended unexpectedly
+//      (though the thread still works)
+// TBD: rm SQL injection (DeviceStatus column Name, StrParam1/StrParam2, etc)
+// TBD: receive types, rm DeviceStatus entries with mismatching types,
+//      ignore params with unknown type values
+// TBD: handle ranges
+// TBD: validate numbers in value handlers
--- hardware/WBHomaBridge.h
+++ hardware/WBHomaBridge.h
@@ -1,25 +1,64 @@
-#pragma once
+#ifndef WBHOMABRIDGE_H
+#define WBHOMABRIDGE_H
 
+#include "stdafx.h"
+#include <map>
 #include <mosquittopp.h>
 #include "DomoticzHardware.h"
+
+struct MQTTAddress
+{
+    MQTTAddress(const std::string& _system_id = "",
+                const std::string& _device_control_id = ""):
+        system_id(_system_id), device_control_id(_device_control_id) {}
+    bool operator<(const MQTTAddress& other) const
+    {
+        return system_id < other.system_id ||
+            (system_id == other.system_id &&
+             device_control_id < other.device_control_id);
+    }
+    std::string GetDeviceID() const {
+        return system_id + "/" + device_control_id;
+    }
+
+    std::string system_id;
+    std::string device_control_id;
+};
+
+struct MQTTValue
+{
+    MQTTValue(const std::string& _value = "", const std::string& _type = ""):
+        value(_value), type(_type) {}
+    bool ready() const {
+        return !value.empty() && !type.empty();
+    }
+
+    std::string value;
+    std::string type;
+};
+
+typedef std::map<MQTTAddress, MQTTValue> ValueMap;
 
 class WBHomaBridge: public CDomoticzHardwareBase
 {
 public:
     WBHomaBridge(const int ID, const std::string IPAddress, const unsigned short usIPPort);
     ~WBHomaBridge();
-	void WriteToHardware(const char *pdata, const unsigned char length);
+    void WriteToHardware(const char *pdata, const unsigned char length);
+    void HandleMQTTMessage(const MQTTAddress& address, bool is_type, const std::string& payload);
 private:
-	bool StartHardware();
-	bool StopHardware();
-    bool DeviceExists();
-    void UpdateDeviceName();
+    bool StartHardware();
+    bool StopHardware();
+    void WriteValueToDB(const MQTTAddress& address, const MQTTValue& value);
     void Do_Work();
 
-	std::string m_szIPAddress;
-	unsigned short m_usIPPort;
+    std::string m_szIPAddress;
+    unsigned short m_usIPPort;
     bool m_stoprequested;
-	boost::shared_ptr<boost::thread> m_thread;
+    boost::shared_ptr<boost::thread> m_thread;
+    ValueMap m_values;
     static bool s_libInitialized;
-    friend class MQTTHandler;
 };
+
+#endif
+
